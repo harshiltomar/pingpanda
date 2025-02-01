3784f5e2dcbdb61c84aa9d4f2d76a35cf8ee9f17
HarshilTomar20 <harshiltomar20@gmail.com>
2025-02-01 19:15:49 +0530

    fix: navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,23 @@
 import Link from "next/link"
 import { MaxWidthWrapper } from "./max-width-wrapper"
 import { SignOutButton } from "@clerk/nextjs"
-import { ArrowRight } from "lucide-react";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "./ui/button"
+import { ArrowRight } from "lucide-react"
+import { currentUser } from "@clerk/nextjs/server"
 
+export const Navbar = async () => {
+  const user = await currentUser()
 
-export const Navbar = () => {
+  return (
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="flex z-40 font-semibold">
+            Ping<span className="text-brand-700">Panda</span>
+          </Link>
 
-    const user = false;
-
-    return (
-        <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
-            <MaxWidthWrapper>
-                <div className="flex h-16 items-center justify-between">
-                    <Link href="/" className="flex z-40 font-semibold">
-                        Ping <span className="text-brand-700">Panda</span>
-                    </Link>
-                </div>
-
-                <div className="h-full flex flex-items space-x-4">
-                {user ? (
+          <div className="h-full flex items-center space-x-4">
+            {user ? (
               <>
                 <SignOutButton>
                   <Button size="sm" variant="ghost">
@@ -71,8 +69,9 @@ export const Navbar = () => {
                 </Link>
               </>
             )}
-                </div>
-            </MaxWidthWrapper>
-        </nav>
-    )
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
 }
